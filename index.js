e7c8487abc1954b2361f715df34df4de5a57fc08
Chebrolu Tejaswi <tejaswichebrolu@gmail.com>
2022-03-03 00:08:27 +0530

    added sequence feature

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/*
-we can also define the funtion at place of handle click.
-1st input= what event we should listen to   2nd=  what it should do once detected
-
-for functions the naming should be followed as abcXyz
-for constructors the naming should be followed as AbcXyz
-*/
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
-
+let sequence=[];
 // when we click on drums
 for(var i=0;i<numberOfDrumButtons;i++)
 {
@@ -14,6 +7,13 @@ for(var i=0;i<numberOfDrumButtons;i++)
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
+    sequence.push(buttonInnerHTML);
+    var seq="";
+    for(let j=0;j<sequence.length;j++)
+    {
+        seq=seq+" "+sequence[j];
+    }
+    document.getElementById("sequence").innerHTML=seq;
   });
 }
 
@@ -21,11 +21,14 @@ for(var i=0;i<numberOfDrumButtons;i++)
 document.addEventListener("keypress",function(event){
   makeSound(event.key);
   buttonAnimation(event.key);
+  sequence.push(key);
+  document.getElementById("sequence").innerHTML=sequence.toString();
 });
 
 
 // to play a sound based on user's input
 function makeSound(key){
+    // console.log(sequence.toString());
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -63,8 +66,22 @@ function makeSound(key){
 function buttonAnimation(currentKey)
 {
   var activeButton= document.querySelector("."+currentKey);
-  activeButton.classList.add("pressed");
+  activeButton.classList.add("pressed"); 
   setTimeout(function(){
     activeButton.classList.remove("pressed");
   },100);
+}
+
+function playSequence(){
+    for(let i=0;i<sequence.length;i++)
+    {        
+        makeSound(sequence[i]);
+        buttonAnimation(sequence[i]);
+    }
+}
+
+function clearsequence(){
+    sequence=[];
+    console.log(sequence);
+    document.getElementById("sequence").innerHTML="...........";   
 }
